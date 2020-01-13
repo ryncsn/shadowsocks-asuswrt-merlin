03a46b2a005f547192b4f55f729c3416c6c9c502
Acris Liu <acrisliu@gmail.com>
2020-01-13 10:00:26 +0800

    No HND anymore

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -3,6 +3,22 @@
 upgrade() {
   SS_MERLIN_HOME=/opt/share/ss-merlin
 
+  # Switch to master branch
+  cd ${SS_MERLIN_HOME} || exit
+  if [[ -f ${SS_MERLIN_HOME}/etc/ss-merlin.conf ]]; then
+    cp ${SS_MERLIN_HOME}/etc/ss-merlin.conf /tmp/ss-merlin/ss-merlin.conf
+  fi
+  if [[ -f ${SS_MERLIN_HOME}/etc/shadowsocks/config.json ]]; then
+    cp ${SS_MERLIN_HOME}/etc/shadowsocks/config.json /tmp/ss-merlin/shadowsocks/config.json
+  fi
+  git checkout master
+  if [[ -f /tmp/ss-merlin/shadowsocks/config.json ]]; then
+    cp /tmp/ss-merlin/ss-merlin.conf ${SS_MERLIN_HOME}/etc/ss-merlin.conf
+  fi
+  if [[ -f /tmp/ss-merlin/shadowsocks/config.json ]]; then
+    cp /tmp/ss-merlin/shadowsocks/config.json ${SS_MERLIN_HOME}/etc/shadowsocks/config.json
+  fi
+
   rm -f ${SS_MERLIN_HOME}/tools/do_upgrade.sh
 
   wget https://cdn.jsdelivr.net/gh/Acris/shadowsocks-asuswrt-merlin@master/tools/do_upgrade.sh -O ${SS_MERLIN_HOME}/tools/do_upgrade.sh
